e83790f4089757bf356f82ffa540e1157aaff4b9
lonerapier <lonerapier@proton.me>
2023-08-22 01:54:00 +0530

    recent notes remove from posts

--- quartz/components/RecentNotes.tsx
+++ quartz/components/RecentNotes.tsx
@@ -33,6 +33,9 @@ export default ((userOpts?: Partial<Options>) => {
     cfg,
   }: QuartzComponentProps) => {
     const opts = { ...defaultOptions(cfg), ...userOpts }
+    if (fileData.slug !== "index") {
+      return <></>
+    }
     const pages = allFiles.filter(opts.filter).sort(opts.sort)
     const remaining = Math.max(0, pages.length - opts.limit)
     return (
